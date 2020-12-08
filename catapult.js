2632305ef240e32a8d320c6483cfdeb7e916398a
ekam06 <73586539+ekam06@users.noreply.github.com>
2020-12-08 19:51:12 +0530

    Add files via upload

---
+++ catapult.js
@@ -0,0 +1,28 @@
+class catapult{
+
+constructor(bodyA,pointB){
+
+var a={
+bodyA:bodyA,
+pointB:pointB,
+length:20,
+stiffness:0.03
+
+
+}
+this.pointB=pointB
+this.c=Constraint.create(a)
+World.add(world,this.c)
+}
+fly(){
+    this.c.bodyA=null
+}
+display(){
+if(this.c.bodyA){
+var pointA=this.c.bodyA.position
+var pointB=this.pointB
+
+line (pointA.x,pointA.y,pointB.x,pointB.y)
+}
+}
+}
